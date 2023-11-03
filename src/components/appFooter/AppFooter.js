f2a07e0e9238fdafa6f62f3be406cf8df4517582
ivan <igorlov18@gmail.com>
2023-11-03 22:04:48 +0100

    changed styles for mobile version

--- src/components/appFooter/AppFooter.js
+++ src/components/appFooter/AppFooter.js
@@ -1,3 +1,16 @@
-const AppFooter = () => {};
+import "./AppFooter.scss";
+
+const AppFooter = () => {
+  return (
+    <div className="app__footer">
+      <div className="app__footer-developer_name">
+        Developed by{" "}
+        <a href="https://www.linkedin.com/in/ivan-horlov-531143276/">
+          Ivan Horlov
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default AppFooter;
